afed471abe75d6d91c8f2049c5d12b61c0a9c3b6
Nikolai Doroshenko <doroshenko.nikolai.a@gmail.com>
2021-08-19 14:24:56 +0300

    add

---
+++ js.js
@@ -0,0 +1,11 @@
+let sectionServices = document.querySelector('.services');
+let buttonBlock = document.querySelectorAll('.services__ul--li');
+let textBlock = document.querySelectorAll('.services__ul--li__text-content');
+let i = 0;
+
+for(i; i<buttonBlock.length; i++) {
+    let k = i;
+    buttonBlock[i].addEventListener('click', () => {
+        textBlock[k].classList.toggle('services__ul--li__text-content--toggle')
+    })
+}
