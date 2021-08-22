544d119fef4e4cf14b2330bfa78cca4333abba3f
Nikolai Doroshenko <doroshenko.nikolai.a@gmail.com>
2021-08-22 16:59:06 +0300

    fix

--- js.js
+++ js.js
@@ -8,3 +8,30 @@ for(i; i<buttonBlock.length; i++) {
             textBlock[k].classList.toggle('services__ul--li__text-content--toggle')
     })
 }
+
+let menuBtn = document.querySelector('.menu__btn');
+let navUl = document.querySelector('.header__nav__ul');
+let navUlItem =document.querySelectorAll('.header__nav__ul--li');
+let bodyOverlay = document.querySelector('.body__overlay');
+let body = document.querySelector('.body');
+
+for (let i=0; i<navUlItem.length; i++) {
+    navUlItem[i].addEventListener ('click', () => {
+        navUl.classList.remove('header__nav-ul-toggle');
+        bodyOverlay.classList.remove('body__overlay-toggle');
+        body.classList.remove('body__overflow');
+    })
+}
+    
+bodyOverlay.addEventListener('click', () => {
+    navUl.classList.remove('header__nav-ul-toggle');
+    body.classList.remove('body__overflow');
+    bodyOverlay.classList.remove('body__overlay-toggle');
+})
+
+menuBtn.addEventListener('click', () => {
+    navUl.classList.toggle('header__nav-ul-toggle');
+    bodyOverlay.classList.toggle('body__overlay-toggle');
+    body.classList.toggle('body__overflow');
+})
+
